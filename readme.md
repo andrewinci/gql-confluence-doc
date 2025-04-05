588daf46454df35ce3e4fe57e7d9725eb9507f50
andrewinci <andrewinci@gmail.com>
2025-04-05 18:30:27 +0100

    retrieve a page from confluence

--- readme.md
+++ readme.md
@@ -1,3 +1,15 @@
 # GQL - Confluence - Doc
 
-A small utility to parse generate Confluence documentation based on a graphql schema.
+A small utility to parse generate Confluence documentation based on a graphq schema.
+
+## Dev
+
+- Confluence API: https://developer.atlassian.com/cloud/confluence/rest/v2/api-group-space/#api-spaces-get
+
+Create a .env file with
+
+```
+CONFLUENCE_DOMAIN="https://<atlassian-domain>.atlassian.net/"
+CONFLUENCE_TOKEN="<a confluence token>"
+CONFLUENCE_USER="< the user email>"
+```
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,56 @@
-console.log("Hello")
+import path from "path";
+
+type ConfluenceConfig = {
+  domain: string;
+  user: string;
+  token: string;
+};
+
+type ConfluencePage = {
+  id: string;
+};
+
+interface ConfluenceClient {
+  getPageById(id: string): Promise<ConfluencePage>;
+}
+
+export function ConfluenceClient(configs: ConfluenceConfig): ConfluenceClient {
+  const headers = {
+    Authorization: `Basic ${Buffer.from(`${configs.user}:${configs.token}`).toString("base64")}}`,
+    Accept: "application/json",
+  };
+
+  return {
+    async getPageById(id: string): Promise<ConfluencePage> {
+      const response = await fetch(
+        path.join(configs.domain, "/wiki/api/v2/pages", id),
+        {
+          method: "GET",
+          headers,
+        },
+      ).then(async (r) => {
+        if (r.status === 200) return r.json();
+        else
+          throw new Error(
+            `Unable to retrieve page ${id}. Error ${r.status}: ${await r.text()} `,
+          );
+      });
+
+      return {
+        id: response.id,
+      };
+    },
+  };
+}
+
+async function main() {
+  const confluenceClient = ConfluenceClient({
+    domain: process.env.CONFLUENCE_DOMAIN!,
+    token: process.env.CONFLUENCE_TOKEN!,
+    user: process.env.CONFLUENCE_USER!,
+  });
+  const page = await confluenceClient.getPageById("131257");
+  console.log("Page found", page);
+}
+
+main();
