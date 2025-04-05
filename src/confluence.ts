a6c925bd9e8d4b5296b42cb461cc5da5733bd5ba
andrewinci <andrewinci@gmail.com>
2025-04-05 19:03:30 +0100

    Add update page function

--- src/confluence.ts
+++ src/confluence.ts
@@ -11,16 +11,40 @@ type ConfluencePage = {
   title: string;
   spaceId: string;
   body: Document;
+  currentVersion: number;
+};
+
+type UpdatePageRequest = {
+  id: string;
+  status: "current" | "draft";
+  title: string;
+  body: {
+    representation: "atlas_doc_format";
+    value: string;
+  };
+  version: {
+    number: number;
+    message: string;
+  };
 };
 
 interface ConfluenceClient {
   getPageById(id: string): Promise<ConfluencePage>;
+  updatePage(
+    id: string,
+    opts: {
+      title: string;
+      body: Document;
+      version: number;
+    },
+  ): Promise<void>;
 }
 
 export function ConfluenceClient(configs: ConfluenceConfig): ConfluenceClient {
   const headers = {
     Authorization: `Basic ${Buffer.from(`${configs.user}:${configs.token}`).toString("base64")}}`,
     Accept: "application/json",
+    "Content-Type": "application/json",
   };
 
   return {
@@ -49,7 +73,42 @@ export function ConfluenceClient(configs: ConfluenceConfig): ConfluenceClient {
         title: response.title,
         spaceId: response.spaceId,
         body: JSON.parse(response.body.atlas_doc_format.value),
+        currentVersion: response.version.number,
       };
+    },
+
+    async updatePage(
+      id: string,
+      opts: {
+        title: string;
+        body: Document;
+        version: number;
+      },
+    ): Promise<void> {
+      const body: UpdatePageRequest = {
+        id,
+        status: "current",
+        version: {
+          number: opts.version,
+          message: "Update from tool",
+        },
+        title: opts.title,
+        body: {
+          value: JSON.stringify(opts.body),
+          representation: "atlas_doc_format",
+        },
+      };
+      await fetch(path.join(configs.domain, "/wiki/api/v2/pages", id), {
+        method: "PUT",
+        headers,
+        body: JSON.stringify(body),
+      }).then(async (r) => {
+        if (r.status === 200) return r.json();
+        else
+          throw new Error(
+            `Unable to update the page ${id}. Error ${r.status}: ${await r.text()} `,
+          );
+      });
     },
   };
 }
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,13 @@ async function main() {
   });
   const page = await confluenceClient.getPageById("294913");
   console.log("Page found", JSON.stringify(page, null, 2));
+  await confluenceClient.updatePage(
+    page.id, {
+      title: "New title",
+      body: page.body,
+      version: page.currentVersion + 1,
+    }
+  )
 }
 
 main();
