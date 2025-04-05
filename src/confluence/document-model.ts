954968d03465e66cfd182f9ff1f9c88f670286cc
andrewinci <andrewinci@gmail.com>
2025-04-05 19:32:23 +0100

    add more types to the ADFDocument

--- src/confluence/document-model.ts
+++ src/confluence/document-model.ts
@@ -1,5 +1,36 @@
 export type ADFDocument = {
   type: "doc";
-  content: unknown;
+  content: Node[];
   version: 1;
+};
+
+type Node = Paragraph | Text | Heading;
+
+/*************** Paragraph node ***************/
+type Paragraph = {
+  type: "paragraph";
+  content: Node[];
+};
+
+/*************** Text node ***************/
+type TextMarkLink = { type: "link"; attrs: { href: string } };
+type TextMarkColor = {
+  type: "textColor";
+  attrs: {
+    color: string;
+  };
+};
+
+type TextMark = TextMarkLink | TextMarkColor;
+type Text = {
+  type: "text";
+  text: string;
+  marks?: TextMark[];
+};
+
+/*************** Text node ***************/
+type Heading = {
+  type: "heading";
+  attrs: { level: 1 | 2 | 3 | 4 };
+  content: Text[];
 };
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,52 @@
 import { ConfluenceClient } from "./confluence/client";
+import { ADFDocument } from "./confluence/document-model";
+
+const samplePage: ADFDocument = {
+  type: "doc",
+  version: 1,
+  content: [
+    {
+      type: "heading",
+      content: [
+        { type: "text", text: "This is a " },
+        {
+          type: "text",
+          text: "title",
+          marks: [{ type: "textColor", attrs: { color: "red" } }],
+        },
+      ],
+      attrs: { level: 1 },
+    },
+    {
+      type: "paragraph",
+      content: [{ type: "text", text: "This is a paragraph" }],
+    },
+    {
+      type: "paragraph",
+      content: [{ type: "text", text: "This is a paragraph 2" }],
+    },
+    {
+      type: "heading",
+      content: [{ type: "text", text: "This an header 2" }],
+      attrs: { level: 2 },
+    },
+    {
+      type: "paragraph",
+      content: [{ type: "text", text: "This is a paragraph 3" }],
+    },
+    {
+      type: "paragraph",
+      content: [
+        { type: "text", text: "This is a paragraph 4" },
+        {
+          type: "text",
+          text: "Title",
+          marks: [{ type: "link", attrs: { href: "#This is a title" } }],
+        },
+      ],
+    },
+  ],
+};
 
 async function main() {
   const confluenceClient = ConfluenceClient({
@@ -8,13 +56,12 @@ async function main() {
   });
   const page = await confluenceClient.getPageById("294913");
   console.log("Page found", JSON.stringify(page, null, 2));
-  await confluenceClient.updatePage(
-    page.id, {
-      title: "New title",
-      body: page.body,
-      version: page.currentVersion + 1,
-    }
-  )
+  // NOTE: when updating any comment will be lost
+  await confluenceClient.updatePage(page.id, {
+    title: "New title",
+    body: samplePage,
+    version: page.currentVersion + 1,
+  });
 }
 
 main();
